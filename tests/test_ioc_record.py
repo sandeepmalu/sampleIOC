5c46122f86fdbfd0e47aabdd6c923d08afa1c6e4
Sandeep Malu <sandeep-kumar.malu@stfc.ac.uk>
2021-04-21 13:52:48 +0530

    add some sleep

--- tests/test_ioc_record.py
+++ tests/test_ioc_record.py
@@ -1,5 +1,6 @@
 import subprocess
 from hamcrest import *
+import time
 
 def execute(cmd):
     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
@@ -11,5 +12,6 @@ def execute(cmd):
         raise subprocess.CalledProcessError(return_code, cmd)
 
 def test_syntax_error_in_IOC_DB_records():
+    time.sleep(7)
     for path in execute(["python","validateDBDir.py","../db/","../dbd/sampleIOC.dbd"]):
         assert_that(path[:8], is_("Checking"))
